5187f7d9b99a239867895f6a1a960e8b495de478
ChristieX <christie.ferns36@gmail.com>
2025-07-14 10:23:45 +0530

    links fixed

--- client/components/header.php
+++ client/components/header.php
@@ -10,10 +10,10 @@
 <body>
     <nav class="navbar navbar-light bg-light shadow-sm border-bottom">
         <div class="container">
-            <a class="navbar-brand text-primary fs-2 border-dark fw-bolder" href="../../Employee-Dashboard">Employee Management System</a>
+            <a class="navbar-brand text-primary fs-2 border-dark fw-bolder" href="/">Employee Management System</a>
             <div class="d-flex align-items-center justify-content-between m-3">
-                <a class="btn btn-primary me-2" href="../../Employee-Dashboard/client/add-employee-form.php" role="button">Add new Employee</a>
-                <a class="btn btn-outline-primary" href="../../Employee-Dashboard" role="button">View Employees</a>
+                <a class="btn btn-primary me-2" href="/client/add-employee-form.php">Add new Employee</a>
+<a class="btn btn-outline-primary" href="/" role="button">View Employees</a>
                 <!-- <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success" type="submit">Search</button> -->
 </div>
